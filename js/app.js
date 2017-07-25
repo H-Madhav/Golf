d68fa8ec602a3b5c12c6b32fd83acee81bd16b81
Hari Madhav Jha <madhav1993hari@gmail.com>
2017-07-25 10:54:03 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
+var i, tabcontent, tablinks, defaultOpen, viewImg, imgModal, close;
+
+tabcontent = document.getElementsByClassName("howTo");
+tablinks = document.getElementsByClassName("tab");
+defaultOpen = document.getElementById("defaultOpen");
+viewImg = document.getElementById('viewImg');
+imgModal = document.getElementById('imgModal');
+close = document.getElementById("close");
+
 function openLearn(evt, task){
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("howTo");
+
 	for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
-    }
+    };
 
-    tablinks = document.getElementsByClassName("tab");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    };
     document.getElementById(task).style.display = "flex";
     evt.currentTarget.className += " active";
 };
 
-document.getElementById("defaultOpen").click();
+
+if (defaultOpen) {
+    defaultOpen.click();
+};
+
+viewImg.onclick = function(){
+    imgModal.style.height = "100%";
+};
+
+close.onclick = function(){
+    imgModal.style.height = "0";
+};
